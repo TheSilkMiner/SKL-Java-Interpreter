bbc8120a616b972b507a2774b049b89add202f9f
TheSilkMiner <sir.karati@outlook.com>
2018-08-05 09:20:49 +0200

    Use Licenser instead of Gradle License Plugin
    Minecrell's version brings a few advantages over the Maven-derived
    plugin we used before. The first and most important one is surely
    the weight of the entire plugin: Minecrell's version does not rely
    on external dependencies and so it provides a quicker download
    experience, especially for CI environments such as Travis, where
    build speed is important. Also, support for Kotlin files is provided
    out-of-the-box, whereas the previous plugin's documentation is
    foggy in this area. At least, when I looked up whether it could be
    used with Kotlin files, not a lot of information came up. One small
    disadvantage of this move is that we lose strict checks, but this
    does not seem like a problem. Third party libraries are instead
    first-class citizens and as such can be specified easily in the
    plugin configuration. I don't know whether this is useful or not
    for us, but it seems something worth looking into.
    
    Signed-off-by: TheSilkMiner <sir.karati@outlook.com>

--- src/main/kotlin/net/thesilkminer/skl/interpreter/skd/Multicatch.kt
+++ src/main/kotlin/net/thesilkminer/skl/interpreter/skd/Multicatch.kt
@@ -5,7 +5,7 @@ package net.thesilkminer.skl.interpreter.skd
 
 import kotlin.reflect.KClass
 
-fun <T> Throwable.multicatch(vararg classes: Class<*>, block: () -> T): T {
+inline fun <T> Throwable.multicatch(vararg classes: Class<*>, block: () -> T): T {
     if (classes.any { this::class.java.isInstance(it) }) {
         return block()
     }
@@ -13,7 +13,7 @@ fun <T> Throwable.multicatch(vararg classes: Class<*>, block: () -> T): T {
 }
 
 @Suppress("NO_REFLECTION_IN_CLASS_PATH", "UNUSED")
-fun <T> Throwable.multicatch(vararg classes: KClass<*>, block: () -> T): T {
+inline fun <T> Throwable.multicatch(vararg classes: KClass<*>, block: () -> T): T {
     if (classes.any { this::class.isInstance(it) }) {
         return block()
     }
